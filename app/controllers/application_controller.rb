5133e35ec00ded9051e95367a7e3fd37bd334ee4
kaishu16 <barcelona_df_5@yahoo.com>
2019-09-19 04:10:13 +0000

    トップページ機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_characters, :first_name_characters, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number])
+  end
+
 end
