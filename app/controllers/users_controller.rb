7aa47a64a676d2918a83a949cc72a4af3d456547
kaishu16 <barcelona_df_5@yahoo.com>
2019-09-19 06:07:33 +0000

    マイページ遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,17 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+
   end
 
   def edit
   end
+
+  private
+  def params_user
+    params.require(:user).permit(:last_name_characters, :first_name_characters, :last_name_kana,
+      :first_name_kana, :profile_image, :postal_code, :address, :phone_number,)
+  end
+
 end
