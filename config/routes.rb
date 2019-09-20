3ed1d0e1fbd8e46b6c142f091c8f644cca3a6f2e
Shunta Watabe <s.ubel.w.w.09@gmail.com>
2019-09-20 13:15:00 +0900

    create admin controller routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root 'users#top'
   devise_for :users
-  devise_for :admins
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
   resources :users, only:[:show, :edit, :update, :destory]
+  resources :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :products
