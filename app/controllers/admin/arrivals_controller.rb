5a3ecfa6a17ddf4913abf7d3d8010569cda4d324
murata-naoya <murata.small.baseball@gmail.com>
2019-09-22 08:48:11 +0000

    arrival作成

--- app/controllers/admin/arrivals_controller.rb
+++ app/controllers/admin/arrivals_controller.rb
@@ -1,7 +1,6 @@
 class Admin::ArrivalsController < ApplicationController
   def new
   	@arrival = Arrival.new
-  	@product = Arrival.products.find_by(params[:product_id])
   end
 
   def create
@@ -16,6 +15,6 @@ class Admin::ArrivalsController < ApplicationController
 
   	private
   	def arrival_params
-  		params.require.(:arrival).permit(:product_id, :arraival_count, :arrival_date)
+  		params.require(:arrival).permit(:product_id, :arraival_count, :arrival_date)
     end
 end
