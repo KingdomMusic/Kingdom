289ed94b37ee06305cc397af8741e17c275bebe4
Shunta Watabe <s.ubel.w.w.09@gmail.com>
2019-09-22 10:36:53 +0900

    admin user index create

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users, only:[:show, :edit, :update, :destory]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
+    resources :users
     resources :products
     resources :artists
     resources :genres
