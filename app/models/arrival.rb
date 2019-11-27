d847a91a70817465e665e6eb93e6681c7b5e8d59
kaishu16 <barcelona_df_5@yahoo.com>
2019-11-27 08:16:33 +0000

    入荷履歴修正

--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -1,4 +1,5 @@
 class Arrival < ApplicationRecord
+	default_scope -> { order(arrival_date: :asc) }
 	belongs_to :product
 	validates :arrival_count, presence:true, numericality: {greater_than: 0}
 	validates :arrival_date, presence:true
